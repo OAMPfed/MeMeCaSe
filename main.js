582d959e9819ec31f0809f64bb1fbd97ebded788
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-05-11 14:14:30 +0800

    When the input is blank, it returns to its original text

--- main.js
+++ main.js
@@ -4,5 +4,16 @@ let output = document.getElementById('memeOutput')
 
 input.addEventListener('input', (e) => {
   let input_value = input.value;
-  output.innerText = memeIt(input_value)
+
+//   Check if the input is empty
+  if(input_value.length == 0){
+
+    // Sets the output text to its original text
+    output.innerText = 'AnD YoUr mEmE TeXt WiLl bE HeRE'
+  }else{
+
+    // Set the value to be the memeIt value
+    output.innerText = memeIt(input_value)
+    
+  }
 })
