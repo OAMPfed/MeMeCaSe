4f2cf893d6c79777f60eb894eb4a0bf73e953328
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-05-11 19:20:39 +0800

    removed all comments in script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-// Main memeIt function
 function memeIt(string) {
-
-  // Converts the whole string to array split it by every letter
   let string_to_array = string.split('')
-
-  // Map every letter (takes 2 args, the currentValue and the index)
   string_to_array.map((letter, index) => {
-
-    // If the index is even
     if(index % 2 == 0){
-
-      // let string_to_array[index] equals to the current letter but uppercase
-
-      // string_to_array[index] is the same as calling string_to_array[0] and so on
-      
       string_to_array[index] = letter.toUpperCase()
     }
   })
-
-  // Returned the joined string
   return string_to_array.join('')
 }
